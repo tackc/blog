6710616e932b231f72193c8fc4119ab9f39eeb5f
Chris Tack <chris_tack@mac.com>
2020-09-01 22:39:52 -0700

    Add state

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,10 +3,7 @@ import React, { useState } from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  const blogPosts = [
-    { title: 'Blog Post #1' },
-    { title: 'Blog Post #2' }
-  ]
+  const [blogPosts, setBlogPosts] = useState([]);
   
   return (
     <BlogContext.Provider value={blogPosts}>
