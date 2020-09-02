fdba4deab0d2b5a1516a7f04a52c2117440335c7
Chris Tack <chris_tack@mac.com>
2020-09-01 22:44:13 -0700

    Create helper function to create new blog post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,6 +4,10 @@ const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
   const [blogPosts, setBlogPosts] = useState([]);
+
+  const addBlogPost = () => {
+    setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}` }]);
+  }
   
   return (
     <BlogContext.Provider value={blogPosts}>
