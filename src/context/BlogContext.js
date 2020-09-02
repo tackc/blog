344e8d5c30c65d6060050a8e4f257a6f2c8d666f
Chris Tack <chris_tack@mac.com>
2020-09-01 22:29:02 -0700

    Create test blog posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,6 +3,11 @@ import React from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
+  const blogPosts = [
+    { title: 'Blog Post #1' },
+    { title: 'Blog Post #2' }
+  ]
+  
   return (
     <BlogContext.Provider value={5}>
       {children}
