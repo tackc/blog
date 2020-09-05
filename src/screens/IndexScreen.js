7ee6cd1b3b5dbe8bd9e6f05891ba3c3d8c01f3fd
Chris Tack <chris_tack@mac.com>
2020-09-04 22:00:33 -0700

    Import and add trash icon

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,20 +3,23 @@ import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 import { Context } from '../context/BlogContext';
 // For ease of use, you can change import variable name at time of import
 // import { Context as blogContext } from '../context, BlogContext';
+import { Feather } from '@expo/vector-icons/';
 
 const IndexScreen = () => {
 const { state, addBlogPost } = useContext(Context);
 
   return (
     <View>
-      <Text>Index Screen</Text>
       <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
         data={state}
         keyExtractor={blogPost => blogPost.title}
         renderItem={({ item }) => {
           return (
-            <Text>{item.title}</Text>
+            <View>
+              <Text>{item.title}</Text>
+              <Feather name="trash-2" />
+            </View>
           )
         }}
       />
