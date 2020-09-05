7c52a1938d474c87eae388d604327b69fa31183d
Chris Tack <chris_tack@mac.com>
2020-09-04 22:58:12 -0700

    Import just navigation from props

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -5,7 +5,7 @@ import { Context } from '../context/BlogContext';
 // import { Context as blogContext } from '../context, BlogContext';
 import { Feather } from '@expo/vector-icons/';
 
-const IndexScreen = () => {
+const IndexScreen = ({ navigation }) => {
 const { state, addBlogPost, deleteBlogPost } = useContext(Context);
 
   return (
