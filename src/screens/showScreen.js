f113a1ef14342de43d420d043cde2f3303af135f
Chris Tack <chris_tack@mac.com>
2020-09-04 23:07:55 -0700

    Add import

--- src/screens/showScreen.js
+++ src/screens/showScreen.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Context } from '../context/BlogContext';
 
 const ShowScreen = ({ navigation }) => {
   (navigation.getParam('id'))
