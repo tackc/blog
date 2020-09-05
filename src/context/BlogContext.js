1541beeb051cbc5fbb4c9a0b063790c59639482e
Chris Tack <chris_tack@mac.com>
2020-09-04 22:26:03 -0700

    Add delete to switch statement

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,6 +2,8 @@ import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
   switch(action.type) {
+    case 'delete_blogpost':
+      return state.filter((blogPost) => blogPost.id !== action.payload );
     case 'add_blogpost':
       return [...state, { id:Math.floor(Math.random() * 99999), title: `Blog Post #${state.length + 1}`}];
     default:
