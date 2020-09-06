38666d304e1af591eed6301de6c2f469cc31a7d8
Chris Tack <chris_tack@mac.com>
2020-09-05 22:19:54 -0700

    Create test post to state

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -24,4 +24,4 @@ const deleteBlogPost = dispatch => {
   }
 }
 
-export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost }, []);
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost }, [{ title: 'TEST POST', content: 'TEST CONTENT', id: 1 }]);
