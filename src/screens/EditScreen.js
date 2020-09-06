cefa707d96e8886d945a2cb21501318c5a97326c
Chris Tack <chris_tack@mac.com>
2020-09-05 22:31:49 -0700

    Create variables

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -3,6 +3,10 @@ import { View, Text, StyleSheet } from 'react-native';
 import { Context } from '../context/BlogContext';
 
 const EditScreen = ({ navigation }) => {
+  const { state } = useContext(Context);
+
+  const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'));
+
   return (
     <View>
       <Text>Edit Screen - {navigation.getParam('id')}</Text>
