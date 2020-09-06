7991cfd1748dceb97e5ebdc5de68ef5ef70ddfee
Chris Tack <chris_tack@mac.com>
2020-09-05 21:34:30 -0700

    Import button

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
 import { Context } from '../context/BlogContext';
 
 const CreateScreen = ({ navigation }) => {
