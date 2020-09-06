5e173556fddbf71b7e777e4b955566cc1926877e
Chris Tack <chris_tack@mac.com>
2020-09-05 22:33:05 -0700

    Create content state

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -8,6 +8,7 @@ const EditScreen = ({ navigation }) => {
   const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'));
 
   const [title, setTitle] = useState(blogPost.title)
+  const [content, setContent] = useState(blogPost.content)
 
   return (
     <View>
