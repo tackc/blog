d32a0385777469942e3076e63f13e390498ed7d1
Chris Tack <chris_tack@mac.com>
2020-09-05 21:34:10 -0700

    Add styling

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -20,11 +20,15 @@ const styles = StyleSheet.create({
   input: {
     fontSize: 18,
     borderWidth: 1,
-    borderColor: 'black'
+    borderColor: 'black',
+    marginBottom: 15,
+    padding: 5,
+    margin: 5
   },
   label: {
     fontSize: 20,
-    marginBottom: 10
+    marginBottom: 5,
+    marginLeft: 5
   }
 });
 
