e7154b4e98038e8b9ae860113a711a1762b1de92
Chris Tack <chris_tack@mac.com>
2020-09-13 21:14:31 -0700

    Fix typo

--- src/components/BlogPostForm.js
+++ src/components/BlogPostForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
-const BlogPostForm = ({ onSubmit, initialValue }) => {
+const BlogPostForm = ({ onSubmit, initialValues }) => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   
