05f17669564f2cafb5d7ba7a76969ac48c33d3a6
Chris Tack <chris_tack@mac.com>
2020-09-13 21:46:48 -0700

    Add optional callback to edit blogpost

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -33,8 +33,11 @@ const deleteBlogPost = dispatch => {
 };
 
 const editBlogPost = dispatch => {
-  return (id, title, content) => {
-    dispatch({ type: 'edit_blogpost', payload: { id, title, content } })
+  return (id, title, content, callback) => {
+    dispatch({ type: 'edit_blogpost', payload: { id, title, content } });
+    if (callback) {
+      callback();
+    }
   };
 };
 
