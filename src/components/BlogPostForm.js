0b69f1a8f87d8e15f0467a215400887fb0475e89
Chris Tack <chris_tack@mac.com>
2020-09-13 21:03:05 -0700

    Pass prop

--- src/components/BlogPostForm.js
+++ src/components/BlogPostForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 
-const BlogPostForm = () => {
+const BlogPostForm = ({ onSubmit }) => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   
